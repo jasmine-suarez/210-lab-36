8a8c0df6164dd3a76ed5ebf126dcae4ead9386a4
Jasmine <jazzyysuarez@gmail.com>
2025-11-29 22:43:31 -0800

    update

--- 210-lab-38-starter.cpp
+++ 210-lab-38-starter.cpp
@@ -4,6 +4,20 @@
 using namespace std;
 
 int main() {
-    
+    StringBinaryTree tree;
+
+    // TEST: inserting strings into tree
+    tree.insertNode("hi");
+    tree.insertNode("my");
+    tree.insertNode("name");
+    tree.insertNode("is");
+    tree.insertNode("jasmine");
+
+    // TEST: displaying tree in order
+    tree.displayInOrder();
+
+    // TEST: using searchNode
+    cout << "Searching for 'name': ";
+
     return 0;
 }
