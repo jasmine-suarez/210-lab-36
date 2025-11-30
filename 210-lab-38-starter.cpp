cbd2e0ab3ea72f1f5a7b28004e736d81f98742c6
Jasmine <jazzyysuarez@gmail.com>
2025-11-29 23:04:37 -0800

    update

--- 210-lab-38-starter.cpp
+++ 210-lab-38-starter.cpp
@@ -24,5 +24,19 @@ int main() {
     tree.displayInOrder();
     cout << endl;
 
+    // menu for user to add, delete, search, and modify records.
+    int choice;
+    do {
+        cout << "Main Menu\n";
+        cout << "[1] \n";
+        cout << "[2] \n";
+        cout << "[3] \n";
+        cout << "[4] \n";
+        cout << "[5] \n";
+        cout << "[6] \n";
+        cout << "[7] \n";
+
+    } while (choice != 0);
+
     return 0;
 }
