9d8963afd6a87c41623e7e8e2a4ccdfdf0d0f09e
Jasmine <jazzyysuarez@gmail.com>
2025-11-29 22:50:45 -0800

    Milestone 2

--- 210-lab-38-starter.cpp
+++ 210-lab-38-starter.cpp
@@ -14,10 +14,31 @@ int main() {
     tree.insertNode("jasmine");
 
     // TEST: displaying tree in order
+    cout << "in order:\n";
     tree.displayInOrder();
+    cout << endl;
+
+    // TEST: displaying tree pre order
+    cout << "pre order:\n";
+    tree.displayPreOrder();
+    cout << endl;
+
+    // TEST: displaying tree post order
+    cout << "post order:\n";
+    tree.displayPostOrder();
+    cout << endl;
 
     // TEST: using searchNode
     cout << "Searching for 'name': ";
+    if (tree.searchNode("name"))
+        cout << "Found" << endl;
+    else
+        cout << "Not Found" << endl;
+
+    // remove a node
+    tree.remove("is");
+    cout << "removed \"is\"\n";
+    tree.displayInOrder();
 
     return 0;
 }
